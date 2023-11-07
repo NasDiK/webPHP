3b5b88f38586891b15186b861bb130e8cbe152e7
NasDiK <tungusov-aleksandr.fl@yandex.ru>
2023-11-07 12:55:29 +0500

    lab 6 done

--- index.php
+++ index.php
@@ -1,36 +1,91 @@
 <?
-  require './config.php';
-  require './utils/logger.php';
+  $lines = file(__DIR__ . '/вар1.csv');
+  $allRows = []; $groupedByVidTransporta = [];
 
-  $pageId = isset($_GET['page']) ? $_GET['page'] : null;
-  $content = null;
-
-  if (isset($pageId)) {
-    $withPad = str_pad($pageId, 2, '0', STR_PAD_LEFT);
-    $lines = file("./pages/Текст$withPad.txt");
-    $linesWithCorrectEncode = array_map(
-      function($line) {
-        return iconv('Windows-1251', 'UTF-8', $line);
-      },
-      $lines
+  foreach($lines as $lineIdx => $lineValue) {
+    $splitted = explode(';', $lineValue);
+    $newRow = array(
+      'vidTransporta' => $splitted[0],
+      'marka' => $splitted[1],
+      'rashod' => $splitted[2]
     );
-  } else {
-    echo "Page не указана";
 
-    return;
+    array_push(
+      $allRows,
+      $newRow
+    );
+
+    if (
+      $lineIdx !== 0 &&
+      !isset(
+        $groupedByVidTransporta[
+          $newRow['vidTransporta']
+        ]
+      )
+    ) {
+      $groupedByVidTransporta[$newRow['vidTransporta']] = [];
+    }
+
+    $lineIdx !== 0 && array_push($groupedByVidTransporta[$newRow['vidTransporta']], $newRow);
   }
 
-  const logger = new Logger(__DIR__ . '/log.txt');
-  logger->info("WATCH_PAGE | $pageId");
-
-  $randomPageNum = rand(1, 10);
-
+  $headers = array_splice($allRows, 0, 1);
 ?>
 
-<a href="./?page=<? echo $randomPageNum ?>"><button>Рандомная страница</button></a>
-
+<select onchange="onChangeVidTransporta(this.value)">
 <?
-  foreach($linesWithCorrectEncode as $key => $line) {
-    echo "<div>$line<div>";
+    foreach(array_keys($groupedByVidTransporta) as $rowIdx => $rowValue) {
+  ?>
+    <option value="<? echo $rowValue; ?>">
+      <? echo $rowValue; ?>
+    </option>
+  <?
+    }
+  ?>
+  <option selected disabled hidden>Вид транспорта</option>
+<select>
+<select id="marka" disabled onchange="onChangeMark(this.value)">
+  <option selected disabled hidden>Марка автомобиля</option>  
+</select>
+<input
+  type="number"
+  min="0"
+  placeholder="Расстояние"
+  onchange="onChangeDistance(this.value)"
+  id="distance"
+/>
+<input type="text" disabled id="rashod" />
+
+<script>
+  const rashod = document.getElementById('rashod');
+  const distance = document.getElementById('distance');
+  let loadedMarks, selectedMark;
+
+  const onChangeDistance = (value) => {
+    const selectedMarkRow = loadedMarks.find(({marka})=> marka === selectedMark);
+
+    rashod.value = Number.parseFloat(selectedMarkRow['rashod']) / 100 * Number.parseFloat(value) + ' л./100 км'
   }
-?>
+
+  const onChangeMark = (value) => {
+    selectedMark = value;
+    distance.disabled = false;
+  }
+
+  const onChangeVidTransporta = (value) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          const result = JSON.parse(this.responseText);
+          loadedMarks = result;
+          
+          document.getElementById("marka").innerHTML = result.reduce((acc, curV) => {
+            return acc + `<option value="${curV['marka']}">${curV['marka']}</option>`;
+          }, '');
+          document.getElementById("marka").disabled = false;
+        }
+      };
+    xhr.open("GET", "actions/getMarks.php?vidTransporta=" + value);
+    xhr.send();
+  }
+</script>
